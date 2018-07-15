76a424c19ca3a555560226fa5a7ee57df9e84282
Peter Schmalfeldt <me@peterschmalfeldt.com>
2018-07-15 17:07:03 -0400

    Initial Commit

---
+++ MMM-Nest-Thermostat.js
@@ -0,0 +1,104 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: MMM-Nest-Thermostat
+ *
+ * By Peter Schmalfeldt
+ * MIT Licensed.
+ */
+
+Module.register('MMM-Nest-Thermostat', {
+	requiresVersion: '2.1.0',
+
+	defaults: {
+		animationSpeed: 0,
+		updateInterval: 60000,
+		whereFilter: null,
+		onlineOnly: true,
+		size: 'large'
+	},
+
+	loaded: false,
+	error: null,
+	thermostats: [],
+
+	start: function() {
+		this.scheduleUpdate(0);
+	},
+
+	updateTemps: function () {
+		var self = this;
+
+		getTemp(this.config, function (response) {
+			if (!response.success || !response.data) {
+				self.error = response.message;
+			} else {
+				self.thermostats = response.data;
+			}
+
+			self.loaded = true;
+			self.updateDom(self.config.animationSpeed);
+		})
+	},
+
+	getScripts: function () {
+		return [
+			this.file('nest.js')
+		];
+	},
+
+	getStyles: function () {
+		return [
+			'MMM-Nest-Thermostat.css'
+		];
+	},
+
+	getDom: function() {
+		var self = this;
+		var wrapper = document.createElement('div');
+
+		if (this.error) {
+			wrapper.innerHTML = '<?xml version="1.0" encoding="UTF-8"?><svg viewBox="0 0 19.5 18.2" width="40" height="40" xmlns="http://www.w3.org/2000/svg"><path transform="translate(-6.2 -6.7)" d="M18.1,24.9H13.8V19.4a2.1,2.1,0,0,1,4.2,0v5.5Zm6-8.1-1.8-1.5v9.5H19.9V19.3a3.9,3.9,0,1,0-7.8,0v5.5H9.6V15.3L7.8,16.8l-1.6-2L16,6.7l4.3,3.6V9.1h2V12l3.4,2.8Z" fill="#666666"/></svg><br />' + this.error;
+			wrapper.className = 'dimmed light small';
+
+			return wrapper;
+		}
+
+		if (!this.loaded) {
+			wrapper.innerHTML = '<?xml version="1.0" encoding="UTF-8"?><svg viewBox="0 0 19.5 18.2" width="40" height="40" xmlns="http://www.w3.org/2000/svg"><path transform="translate(-6.2 -6.7)" d="M18.1,24.9H13.8V19.4a2.1,2.1,0,0,1,4.2,0v5.5Zm6-8.1-1.8-1.5v9.5H19.9V19.3a3.9,3.9,0,1,0-7.8,0v5.5H9.6V15.3L7.8,16.8l-1.6-2L16,6.7l4.3,3.6V9.1h2V12l3.4,2.8Z" fill="#666666"/></svg><br />...';
+			wrapper.className = 'dimmed light small';
+
+			return wrapper;
+    }
+
+		for (var i = 0; i < this.thermostats.length; i++) {
+
+			var dial = document.createElement('div');
+			dial.className = 'thermostat thermostat__' + this.config.size;
+
+			var thermostat = new thermostatDial(dial, {
+				size: self.config.size,
+				temperature_scale: this.thermostats[i].temperature_scale
+			});
+
+			thermostat.where_name = this.thermostats[i].where_name;
+			thermostat.has_leaf = this.thermostats[i].has_leaf;
+			thermostat.hvac_state = this.thermostats[i].hvac_state;
+			thermostat.target_temperature = this.thermostats[i].target_temperature;
+			thermostat.ambient_temperature = this.thermostats[i].ambient_temperature;
+
+			wrapper.appendChild(dial);
+		}
+
+		return wrapper;
+	},
+
+	scheduleUpdate: function() {
+		var self = this;
+		var delay = (!this.loaded) ? 0 : this.config.updateInterval;
+
+		setTimeout(function() {
+			self.updateTemps();
+		}, delay);
+	}
+});
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # MMM-Nest-Thermostat
-Magic Mirror Nest Thermostat
+
+This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror/).
+
+Add Nest Thermostats to Magic Mirror.
+
+![screenshot](https://peter.build/magic-mirror/mmm-nest-thermostat.png)
+
+## Installation
+
+```bash
+cd /path/to/MagicMirror
+git clone https://github.com/manifestinteractive/MMM-Nest-Thermostat ./modules/MMM-Nest-Thermostat
+```
+
+## Generate Nest Token
+
+You will need to run this for the `config.token` param.
+
+```bash
+cd ./modules/MMM-Nest-Thermostat
+./getNestToken.sh
+```
+
+## Using the module
+
+To use this module, add the following configuration block to the modules array in the `config/config.js` file:
+
+```js
+var config = {
+  modules: [
+    {
+      module: 'MMM-Nest-Thermostat',
+      position: 'lower_third',
+      config: {
+        token: 'xxxxxxxxxx',
+        size: 'medium',
+        onlineOnly: true,
+        whereFilter: ['Downstairs']
+      }
+    }
+  ]
+}
+```
+
+## Configuration options
+
+Option           | Type      | Default | Description
+-----------------|-----------|---------|--------------------------------------------------------------
+`animationSpeed` | `int`     | `0`     | Fade In Animation Speed
+`updateInterval` | `int`     | `60000` | Frequency Update
+`whereFilter`    | `array`   | `null`  | Only show Thermostats with these names
+`onlineOnly`     | `boolean` | `true`  | Only show Thermostats that are currently online
+`size`           | `string`  | `large` | One of the following sizes: `large`, `medium`, `small`, `mini`
